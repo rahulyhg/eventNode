1a799e90491efd802e7f1a906fa35aee40867540
sayalighule <sayali.ghule@wohlig.com>
2017-12-04 12:44:50 +0530

    social login added

--- backend/js/controllers/controller.js
+++ backend/js/controllers/controller.js
@@ -11,7 +11,7 @@ myApp.controller('DashboardCtrl', function ($scope, TemplateService, NavigationS
         if ($.jStorage.get("accessToken")) {
 
         } else {
-            // $state.go("login");
+            $state.go("login");
         }
     })
     .controller('JagzCtrl', function ($scope, TemplateService, NavigationService, $timeout, $state, $interval) {
--- config/passport/google.js
+++ config/passport/google.js
@@ -1,6 +1,11 @@
+// global["GoogleKey"] = "AIzaSyDMgwXi6D38isibUfShc9C2mJyaHZZ2LpE";
+// global["GoogleclientId"] = "529279279497-hdm2ul03erq4kitk7qlqbf41h6pl8f7p.apps.googleusercontent.com";
+// global["GoogleclientSecret"] = "nNZiqXW5U2364QI9--sVIR8B";
+
+
 global["GoogleKey"] = "AIzaSyDMgwXi6D38isibUfShc9C2mJyaHZZ2LpE";
-global["GoogleclientId"] = "529279279497-hdm2ul03erq4kitk7qlqbf41h6pl8f7p.apps.googleusercontent.com";
-global["GoogleclientSecret"] = "nNZiqXW5U2364QI9--sVIR8B";
+global["GoogleclientId"] = "1008216748494-fjmtqcv1rfiqqko1642uer7kukpjn8u2.apps.googleusercontent.com";
+global["GoogleclientSecret"] = "hljBEegCChORL7EtQHcUeW3t";
 
 passport.use(new GoogleStrategy({
         clientId: GoogleclientId,
