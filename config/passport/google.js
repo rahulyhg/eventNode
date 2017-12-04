47fd73dd634c2046fb59583d3f337bc742a1c3d4
sayalighule <sayali.ghule@wohlig.com>
2017-12-04 19:03:38 +0530

    new client secret

--- config/passport/google.js
+++ config/passport/google.js
@@ -5,7 +5,7 @@
 
 global["GoogleKey"] = "AIzaSyDMgwXi6D38isibUfShc9C2mJyaHZZ2LpE";
 global["GoogleclientId"] = "634210333967-mp0e7569cjco754h9300gjs14ngcf8np.apps.googleusercontent.com";
-global["GoogleclientSecret"] = "ylfagaWN0lvWVlqv1onG2_PS";
+global["GoogleclientSecret"] = "OmQ1yT5StzwBzxqFCy95PydN";
 
 // passport.use(new GoogleStrategy({
 //         clientId: GoogleclientId,
